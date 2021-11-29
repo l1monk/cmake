49ac98ff517771e82e03672b7744f4fa9b814590
l1monk <teodor.evpatov@yadex.ru>
2021-11-29 20:44:25 +0300

    innet

---
+++ CMakeFiles/MyFirstCmakeProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFirstCmakeProject.dir/main.cpp.o"
+  "CMakeFiles/MyFirstCmakeProject.dir/main.cpp.o.d"
+  "MyFirstCmakeProject.exe"
+  "MyFirstCmakeProject.pdb"
+  "libMyFirstCmakeProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyFirstCmakeProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+void print(){
+    cout << "I know that I use keyboard and mouse" << endl;
+}
+
+int main(int argc, char const *argv[]){
+    print();
+    
+    return 0;
+}
